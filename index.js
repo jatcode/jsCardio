385eda3c505ffb22c4cedfb077550e03e959bc1d
Jesus Amaya <yokodevmaster2015@gmail.com>
2018-02-27 16:38:51 -0600

    Finished the excercises

--- index.js
+++ index.js
@@ -80,19 +80,69 @@ function reverseInt(int) {
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) {}
+function capitalizeLetters(str) {
+  let capitalizedStr = ''
+  capitalizedStr = str.toLowerCase().split(' ').map(
+    word=> word[0].toUpperCase()+ word.substr(1)
+  ).join(' ')
+  
+  return capitalizedStr
+}
 
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) {
+  let maxChar=0
+  let mostCommonChar=''
+  var objAppearances= str.split('').reduce((acc, currChar)=>{
+    if(currChar in acc){
+      acc[currChar]++
+    }else{
+      acc[currChar]=1
+    }
+    return acc
+  },{})
+  for(const letra in objAppearances){
+    // console.log(`${letra}:${objAppearances[letra]}`)
+    if(objAppearances[letra] >maxChar){
+      maxChar = objAppearances[letra]    
+      mostCommonChar=letra    
+    }
+  }
+  return mostCommonChar
+  // for(const character of acc) {
+  //   console.log('demas ',character)
+  // }
+  
+
+}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
-// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+// Write a program that prints all the numbers from 1 to 100. 
+// For multiples of 3, instead of the number, print "Fizz", 
+// For multiples of 5 print "Buzz". 
+// For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+function fizzBuzz() {
+  for(let i=1; i<=100; i++){
+    if(i % 5===0 && i % 3===0) {
+      console.log('FizzBuzz')
+    }else
+    if(i%3===0){
+      console.log('Fizz')
+    }else 
+    if(i%5===0){ 
+      console.log('Buzz')
+    }else{
+      console.log(i)
+
+    }
+  }
+  return 'Done!!'  
+}
 
 
 
@@ -102,6 +152,10 @@ function fizzBuzz() {}
 // const output = isPalindrome('racecar');
 // const output = isPalindrome('madam');
 // const output = reverseInt(12345);// it works as it should
-const output = reverseInt(-12345);
+// const output = reverseInt(-12345);
+// const output = capitalizeLetters('la primera oracion del dia');
+// const output = maxCharacter('primera')
+// const output = maxCharacter('javascripttttt')
+const output = fizzBuzz('javascripttttt')
 
 console.log(output);
